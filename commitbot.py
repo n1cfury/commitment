296dd97b322482b239966c071967aa503065059d
n1cfury <n1cfury@users.noreply.github.com>
2023-04-03 10:48:29 -0700

    Create commitbot.py
    The script

---
+++ commitbot.py
@@ -0,0 +1,18 @@
+import os
+import random
+
+# specify path to local repository
+repo_path = 'https://github.com/n1cfury/commitment'
+
+# specify number of commits to generate
+num_commits = random.randint(3, 19)
+
+# generate and commit random changes
+for i in range(num_commits):
+    # generate random text file
+    with open(os.path.join(repo_path, 'random.txt'), 'w') as f:
+        f.write(str(random.randint(0, 1000000)))
+    # stage and commit changes
+    os.system('cd {0} && git add -A && git commit -m "Random commit #{1}"'.format(repo_path, i+1))
+    
+print('Generated {0} random commits.'.format(num_commits))
